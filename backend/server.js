d7f63586eb1724138fc38b2707519d2bb0de6190
IT22074072 <it22074072@my.sliit.lk>
2025-05-01 23:04:13 +0530

    last changes

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,11 @@ const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const favoriteRoutes = require('./routes/favoriteRoutes');
 
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/favorites', favoriteRoutes);
@@ -23,8 +18,5 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+// ✅ Export the app for Vercel
+module.exports = app;
