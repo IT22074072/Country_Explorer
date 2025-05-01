95deaaf475f2d4cc8315423c684b30d7899ec79d
IT22074072 <it22074072@my.sliit.lk>
2025-05-01 16:16:32 +0530

    add some

--- country-app/src/services/favoriteService.js
+++ country-app/src/services/favoriteService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/favorites';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Helper function to get authorization header
 const getAuthHeader = () => {
